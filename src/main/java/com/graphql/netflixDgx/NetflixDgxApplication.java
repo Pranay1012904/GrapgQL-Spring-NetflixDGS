e44c2e0cfc795720bb70cb2aab6b2466c23449c9
Pranay1012904 <pranay.dmnsns@gmail.com>
2026-03-09 22:47:33 +0530

    Initialization

---
+++ src/main/java/com/graphql/netflixDgx/NetflixDgxApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.netflixDgx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixDgxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixDgxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql/netflixDgx/NetflixDgxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.netflixDgx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixDgxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
